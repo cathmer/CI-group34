720287b0e508669adae4fc513a4eba49356ec8be
Casper Athmer <casperathmer92@gmail.com>
2015-09-15 19:05:24 +0200

    finished commenting

--- src/Code.m
+++ src/Code.m
@@ -32,10 +32,10 @@ outputWeights = (2.4/HIDDENNEURONS -(-2.4/HIDDENNEURONS))*rand(HIDDENNEURONS, 7)
 tresholdOutputNeurons = (2.4/HIDDENNEURONS -(-2.4/HIDDENNEURONS))*rand(1, 7) + (-2.4/HIDDENNEURONS);
 
 % The number of times the complete training set will be looped through
-EPOCHS = 10;
+EPOCHS = 50;
 % An array, initliazed with all zeros, which will contain the Mean Squared
 % Error of each epoch
-allErrors = zeros(EPOCHS);
+allErrors = zeros(EPOCHS, 1);
 
 %% The learning-phase starts here
 % The number of times one epoch is executed
@@ -68,42 +68,67 @@ for iterations = 1: EPOCHS
         
         % Initialize a column vector of size HIDDENNEURONS, with all zeros
         errorsHiddenLayer = zeros(HIDDENNEURONS,1);
-
+    
+        % Loops over the outputs of all the output neurons
         for k=1: size(output)
+            % Calculates the gradient for the current output neuron
             gradient = output(k)*(1-output(k))*errors(k);
+            % Updates the treshold of the current neuron
             tresholdOutputNeurons(k) = tresholdOutputNeurons(k) + (alpha * -1 * gradient);
-
+    
+            % Loops over the column in the weights matrix (outputweights) corresponding to
+            % the current neuron (column k)
             for j=1: size(outputWeights, 1)
+                % The value of the current field in the matrix
                 currentWeight = outputWeights(j,k);
+                % Updates the weight of the field corresponding to (j,k) in
+                % the matrix
                 outputWeights(j, k) = currentWeight + (alpha * hiddenNeuronsOutput(j) * gradient);
+                
+                % The old weight times the gradient is added to the
+                % errorvalue of the hidden neuron that corresponds to the 
+                % current weight.
                 errorsHiddenLayer(j) = errorsHiddenLayer(j) + currentWeight*gradient;
             end
         end
-
+    
+        % Transposes the output of the hidden neurons into a column vector
         hiddenOutputTransposed = transpose(hiddenNeuronsOutput);
-
+        
+        % Loops over all the outputs of the hidden neurons
         for h=1: size(hiddenOutputTransposed)
+            % Calculates the gradient of the current hidden neuron
             gradient = hiddenOutputTransposed(h)*(1-hiddenOutputTransposed(h))*errorsHiddenLayer(h);
+            % Updates the treshold of the current hidden neuron
             tresholdHiddenNeurons(h) = tresholdHiddenNeurons(h) + alpha* -1 * gradient;
-
+            
+            % Loops over the column in the weights matrix (inputweights) corresponding to
+            % this neuron (column h)
             for i=1: size(inputWeights, 1)
-                currentWeight = inputWeights(i, h);
-                inputWeights(i, h) = currentWeight + (alpha * input(i) * gradient);
+                % Updates the weight for the field corresponding to (i, h)
+                inputWeights(i, h) = inputWeights(i, h) + (alpha * input(i) * gradient);
             end
         end
-
+        
+        % Calculates the sum of all errors squared for the current output
+        % and ads it to errorsSquared
         for e=1: size(errors)
             errorsSquared = errorsSquared + (errors(e)^2);
         end
     end
     
+    % Calculate the mean squared error of the last Epoch
     MSE = 1/size(Features,1)*errorsSquared;
     disp(MSE);
-    allErrors = [allErrors; MSE];
     
+    % Add the MSE to a vector containing all MSE's for all epochs
+    allErrors(iterations) = MSE;
+    
+    % Stop if the MSE of the last epoch is smaller than 0.1
     if MSE < 0.1
         break;
     end
 end
 
+% Plot all the MSE's
 plot(allErrors);
