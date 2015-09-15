325c0c683e1e8cce82e5c2d515acb8c6088c9fed
Bastijn Kostense <b.kostense@student.tudelft.nl>
2015-09-15 15:32:18 +0200

    Add stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CI-group34
+The link to the report in Google Docs: https://docs.google.com/document/d/1t3hUN-20K-E9kUiwq4CszPOG5mJm22H2I0iH-F6Znr8/edit?usp=sharing
--- src/Code.m
+++ src/Code.m
@@ -1,5 +1,19 @@
- Features = importdata('features.txt')
- Targets = importdata('targets.txt')
- Unknown = importdata('unknown.txt')
+ %Loads text files into matrices
+ Features = importdata('features.txt');
+ Targets = importdata('targets.txt');
+ Unknown = importdata('unknown.txt');
+ 
+ HIDDENNEURONS = 8;
+ 
+ %Generates random weight vectors
+ inputWeights = (2.4/10 -(-2.4/10))*rand(10, HIDDENNEURONS) + (-2.4/10);
+ outputWeights = (2.4/HIDDENNEURONS -(-2.4/HIDDENNEURONS))*rand(HIDDENNEURONS, 7) + (-2.4/HIDDENNEURONS);
  
  
+ %The learning-phase starts here
+ %Loops through all the rows of Features
+ for i = 1: size(Features,1)
+       input = Features(i,:);
+       hiddenLayerNeurons = input * inputWeights;
+       
+ end
