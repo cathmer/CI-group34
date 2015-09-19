db43041be261bc2164b850711455fa914251f264
Casper Athmer <casperathmer92@gmail.com>
2015-09-19 11:45:24 +0200

    cleaned up code a little, also ran the code and wrote output of unknown set to file

--- src/targetsToVec.m
+++ src/targetsToVec.m
@@ -1,13 +1,16 @@
+%% Code to convert the Targets into a matrix which can be used by the MatLab Toolbox for Neural Networks
+
 clc;
 clear all;
 
+% Import the targets
 Targets = importdata('targets.txt'); 
 
 TargetsVec = [];
 
+% For each item of the targets, transpose it and convert it into a vector
+% with 6 zero's and 1 one
 for i=1: size(Targets,1)
     vec = transpose(ind2vec(Targets(i), 7));
     TargetsVec = [TargetsVec; vec];
 end
-
-TargetsVec;
